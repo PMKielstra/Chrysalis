1a719702231b4fbee0da19960c7df0db9c098131
Michael Kielstra <pmkielstra@users.noreply.github.com>
2023-06-27 14:40:17 -0700

    Working better now!

--- TreeBasedMatrix.py
+++ TreeBasedMatrix.py
@@ -7,7 +7,7 @@ import itertools
 import matplotlib.pyplot as plt
 
 N = 1024
-eps = 1e-7
+eps = 1e-10
 
 def K_from_coords(coords_list):
     coords = np.meshgrid(*coords_list, indexing='ij')
@@ -19,7 +19,6 @@ def K_from_coords(coords_list):
 
 def ss_row_id(sampled_ranges, factor_index):
     """Carries out a subsampled row ID for a tensor, unfolded along factor_index."""
-
     # Step 2: Set up a tensor from the points chosen by the subsampling
     A = K_from_coords(sampled_ranges)
 
@@ -34,46 +33,65 @@ def ss_row_id(sampled_ranges, factor_index):
     return new_rows, R.T
 
 class FactorTree:
-    def __init__(self, rows, matrix):
-        self.matrix = matrix
-        self.rows = rows
+    def __init__(self, rows_mats):
+        self.rows_mats = rows_mats
         self.children = []
+        self.down_results = []
 
-def factor_to_tree(rows_list, multirange, factor_index, levels):
-    new_rows, Rs = [], []
-    for r in rows_list:
-        row, R = ss_row_id(multirange.overwrite(SliceTree(r), factor_index), factor_index)
-        new_rows.append(row)
-        Rs.append(R)
-    tree = FactorTree(np.concatenate(new_rows), block_diag(*Rs))
-    merged_rows = [np.concatenate((a, b)) for a, b in zip(new_rows[::2], new_rows[1::2])]
-    if levels > 0:
-        tree.children = [factor_to_tree(merged_rows, step, factor_index, levels - 1) for step in multirange.next_steps()]
+def factor_to_tree(rows_lists, multirange, factor_axis, level):
+    rows_mats = [ss_row_id(multirange.overwrite(SliceTree(r), factor_axis), factor_axis) for r in rows_lists]
+    new_rows = [np.concatenate((p[0], q[0])) for p, q in zip(rows_mats[::2], rows_mats[1::2])]
+    tree = FactorTree(rows_mats)
+    if level > 0:
+        tree.children = [factor_to_tree(new_rows, next_range, factor_axis, level - 1) for next_range in multirange.next_steps()]
     return tree
 
-def apply_down_tree(A, tree, factor_index):
-    new_A = np.matmul(A, tree.matrix)
+def apply_down(split_A, tree, factor_axis):
+    results = []
+    for row_mat, A_1, A_2 in zip(tree.rows_mats, split_A[::2], split_A[1::2]):
+        new_A = np.matmul(row_mat[1].T, np.concatenate((A_1, A_2)))
+        results.append((row_mat[0], new_A))
     if tree.children == []:
-        return [(tree.rows, new_A)]
-    return [i for l in (apply_down_tree(new_A, c, factor_index) for c in tree.children) for i in l]
+        return [results]
+    child_results = [apply_down([r[1] for r in results], child, factor_axis) for child in tree.children]
+    return [c for sublist in child_results for c in sublist]
 
-def apply_up_tree(row_list, tree, factor_index):
+def list_transpose(l):
+    return [[ll[i] for ll in l] for i in range(len(l[0]))]
+
+def propagate_down(transposed_lists, tree):
     if tree.children == []:
-        print(sum(len(r) * len(tree.rows) for r in row_list))
-        matrix_row = [np.matmul(A, K_from_coords([r, tree.rows])) for r, A in row_list]
+        assert len(transposed_lists) == 1
+        for tree_rows_mats, down_rows_mats in zip(tree.rows_mats, transposed_lists[0]):
+            print(len(tree_rows_mats[0]) * len(down_rows_mats[0]))
+        tree.down_results = [K_from_coords((tree_rows_mats[0], down_rows_mats[0])).dot(down_rows_mats[1]) for tree_rows_mats, down_rows_mats in zip(tree.rows_mats, transposed_lists[0])]
+        return
+    for i, child in enumerate(tree.children):
+        propagate_down(transposed_lists[i*(len(transposed_lists) // len(tree.children)) : (i+1) * (len(transposed_lists) // len(tree.children))], child)
+
+def propagate_up(tree):
+    if tree.children == []:
+        split_As = tree.down_results
+        return block_diag(*[rm[1] for rm in tree.rows_mats]).dot(np.concatenate(split_As))
     else:
-        matrix_row = [apply_up_tree(row_list, c, factor_index) for c in tree.children]
-    matrix = sum(matrix_row) / len(matrix_row)
-    return np.matmul(matrix, tree.matrix.T)
+        split_As = [propagate_up(child) for child in tree.children]
+    return block_diag(*[rm[1] for rm in tree.rows_mats]).dot(sum(split_As))
 
-mr_rows = Multirange([SliceTree(list(range(N))), SliceTree(list(range(N)))], [2, 0])
-mr_cols = Multirange([SliceTree(list(range(N))), SliceTree(list(range(N)))], [0, 2])
-split_range = np.array_split(list(range(N)), 128)
 
-A = np.random.rand(1, N)
+mr_rows = Multirange([SliceTree(list(range(N))), SliceTree(list(range(N)))], [0, 2])
+mr_cols = Multirange([SliceTree(list(range(N))), SliceTree(list(range(N)))], [2, 0])
+split_ranges = np.array_split(list(range(N)), 256)
+A = np.random.rand(N)
+split_A = np.array_split(A, 512)
 
-lst = apply_down_tree(A, factor_to_tree(split_range, mr_rows, 0, 3), 0)
-compressed_K = apply_up_tree(lst, factor_to_tree(split_range, mr_cols, 1, 3), 1)
-true_K = np.matmul(A, K_from_coords([list(range(N)), list(range(N))]))
-plt.spy(compressed_K - true_K)
-print(np.linalg.norm(compressed_K - true_K) / np.linalg.norm(true_K))
+tree_rows = factor_to_tree(split_ranges, mr_rows, 0, 4)
+k = list_transpose(apply_down(split_A, tree_rows, 0))
+
+tree_cols = factor_to_tree(split_ranges, mr_cols, 1, 4)
+propagate_down(k, tree_cols)
+compressed_A = propagate_up(tree_cols)
+
+true_A = K_from_coords([list(range(N)), list(range(N))]).dot(A)
+
+print(np.linalg.norm(compressed_A - true_A) / np.linalg.norm(true_A))
+
