9a4ead423524463f2fcf1386e9626f609f61c5fa
Michael Kielstra <pmkielstra@users.noreply.github.com>
2023-07-07 12:12:45 -0700

    Parallel processing

--- TreeBasedTensor/factor.py
+++ TreeBasedTensor/factor.py
@@ -1,3 +1,6 @@
+from concurrent.futures import Future
+import time
+
 import numpy as np
 import scipy.linalg.interpolative as interp
 import tensorly
@@ -13,7 +16,7 @@ UP = -1
 class Profile:
     """Stores all the parameters necessary for a factorization."""
 
-    def __init__(self, N, dimens, eps, levels, direction=BOTH):
+    def __init__(self, N, dimens, eps, levels, direction=BOTH, processes=None):
         assert N > 0
         assert dimens > 0
         assert eps < 1
@@ -24,6 +27,7 @@ class Profile:
         self.eps = eps
         self.levels = levels
         self.direction = direction
+        self.processes = processes
         self.factor_source = 0 # TODO: add the possibility to factor along different axes.
         self.factor_observer = dimens
         self.off_split_number = 2 ** levels if direction == BOTH else 1
@@ -81,28 +85,47 @@ def one_level_factor(profile, rows_lists, off_cols, passive_multirange, is_sourc
     
     return rows_mats, new_rows
 
-def factor_to_tree(profile, rows_lists_source, rows_lists_observer, off_cols, passive_multirange, level, root=True):
+def factor_to_tree(profile, off_cols, rows_lists_source=None, rows_lists_observer=None, passive_multirange=None, level=None, root=True):
     """Recursively create a factor tree which goes either down, up, or both.  Decrements level every step and stops when it hits zero, allowing for partial factorizations."""
+    if rows_lists_source is None:
+        rows_lists_source = np.array_split(range(profile.N), 2 ** (2 * profile.levels if profile.direction == BOTH else profile.levels))
+    if rows_lists_observer is None:
+        rows_lists_observer = np.array_split(range(profile.N), 2 ** (2 * profile.levels if profile.direction == BOTH else profile.levels))
+    if passive_multirange is None:
+        passive_multirange = Multirange([SliceTree(list(range(profile.N))) for _ in range(profile.dimens)], [2 for _ in range(profile.dimens)])
+    if level is None:
+        level = profile.levels
+    
     rows_mats_source, new_rows_source = (None, None) if profile.direction == UP else one_level_factor(profile, rows_lists_source, off_cols, passive_multirange, is_source=True)
     rows_mats_observer, new_rows_observer = (None, None) if profile.direction == DOWN else one_level_factor(profile, rows_lists_observer, off_cols, passive_multirange, is_source=False)
     
     tree = FactorTree(rows_mats_source, rows_mats_observer, passive_multirange.position(), root)
     
     if level > 0:
-        tree.children = [factor_to_tree(profile, new_rows_source, new_rows_observer, off_cols, next_step, level - 1, False) for next_step in passive_multirange.next_steps()]
-    
+        tree.children = [factor_to_tree(profile, off_cols, new_rows_source, new_rows_observer, next_step, level - 1, False) for next_step in passive_multirange.next_steps()]
+
     return tree
 
-def build_factor_forest(profile):
+def single_arg_factor_to_tree(data):
+    return factor_to_tree(*data)
+
+def build_factor_forest(pool, profile):
     """Build a "factor forest": a tuple (levels, off_cols_lists, trees, direction), giving the number of levels of factorization, the lists of columns for each tree, the list of actual factor trees, and the direction of factorization (UP, DOWN, or BOTH)."""
 
     off_cols_lists = np.array_split(range(profile.N), profile.off_split_number)
-    rows_list = np.array_split(range(profile.N), 2 ** (2 * profile.levels if profile.direction == BOTH else profile.levels))
-    passive = Multirange([SliceTree(list(range(profile.N))) for _ in range(profile.dimens)], [2 for _ in range(profile.dimens)])
-
+    
     def make_trees(previous_off_cols, level):
         if level == 0:
-            return factor_to_tree(profile, rows_list, rows_list, previous_off_cols, passive, profile.levels)
-        return [make_trees(previous_off_cols + [off_cols], level - 1) for off_cols in off_cols_lists]
+            return pool.submit(factor_to_tree, profile, previous_off_cols), 1
+        trees_and_counts = [make_trees(previous_off_cols + [off_cols], level - 1) for off_cols in off_cols_lists]
+        return [tc[0] for tc in trees_and_counts], sum(tc[1] for tc in trees_and_counts)
+
+    def get_all_pool_results(nested_lists):
+        if isinstance(nested_lists, Future):
+            return nested_lists.result()
+        return [get_all_pool_results(l) for l in nested_lists]
+
+    trees_async, counts = make_trees([], profile.dimens - 1)
+    trees = get_all_pool_results(trees_async)
             
-    return off_cols_lists, make_trees([], profile.dimens - 1)
+    return off_cols_lists, trees
--- TreeBasedTensor/main.py
+++ TreeBasedTensor/main.py
@@ -1,6 +1,11 @@
 import time
+import sys
+from concurrent.futures import ProcessPoolExecutor
+
 import numpy as np
 from tqdm import tqdm
+from mpipool import MPIExecutor
+
 from factor import Profile, build_factor_forest, UP, DOWN, BOTH
 from matvec import apply
 from tensor import AK_true
@@ -18,11 +23,16 @@ def tock():
 dimens = 3 # Number of source or observer dimens, not source + observer dimens
 eps = 1e-6
 
+MPI = len(sys.argv) > 1 and sys.argv[1] == "--mpi"
+PoolExecutor = MPIExecutor if MPI else ProcessPoolExecutor
+
 import csv
-with open(f"profile_{dimens}d.csv", mode="w", newline="") as csvfile:
+with open(f"profile_{dimens}d.csv", mode="w", newline="") as csvfile, PoolExecutor() as pool:
+    if MPI:
+        pool.workers_exit()
     writer = csv.writer(csvfile)
     writer.writerow(["N", "Time to factor", "Time to compress", "Total size", "Max rank"])
-    logNs = [2, 4, 6]
+    logNs = [4]
     for logN in tqdm(logNs):
         N = 2 ** (logN)
         A = np.random.rand(* [N] * dimens)
@@ -34,7 +44,7 @@ with open(f"profile_{dimens}d.csv", mode="w", newline="") as csvfile:
             direction = BOTH
             )
         tick()
-        factor_forest = build_factor_forest(profile)
+        factor_forest = build_factor_forest(pool, profile)
         ttf = tock()
         ts = total_memory(profile, factor_forest)[0]
         mr = max_leaf_row_length_forest(factor_forest)
