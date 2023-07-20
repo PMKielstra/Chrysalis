85e59d54b773ce1c9a6cabe613f7c002eb14ddc4
Michael Kielstra <pmkielstra@users.noreply.github.com>
2023-07-20 15:54:53 -0700

    New as-matrix arrangement

--- TreeBasedTensor/tensor.py
+++ TreeBasedTensor/tensor.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 def mod_stack(profile, coords):
-    stack = [np.mod(coords, profile.true_N)]
-    for _ in range(profile.true_dimens - 1):
-        coords = (coords - stack[-1]) / profile.true_N
-        stack.append(np.mod(coords, profile.true_N))
-    return np.stack(stack, axis=0)
+    stack = [np.mod(coords, 2)]
+    for _ in range(profile.true_dimens * (profile.levels + 3)):
+        coords = (coords - stack[-1]) // 2
+        stack.append(np.mod(coords, 2))
+    assert max(np.ravel(coords)) == 0
+    split_stacks = [stack[i::profile.true_dimens] for i in range(profile.true_dimens)]
+    split_pos = [sum(2^i * x for i, x in enumerate(s)) for s in split_stacks]
+    return np.stack(split_pos, axis=0)
 
 def K_from_coords(profile, coords_list):
     """Get a subtensor of K."""
@@ -19,7 +22,7 @@ def K_from_coords(profile, coords_list):
         leftstack = np.stack(coords[:halflen], axis=0)
         rightstack = np.stack(coords[halflen:], axis=0)
     norm = np.sqrt((profile.dsquared) + np.sum(((leftstack - rightstack) / (profile.true_N - 1)) ** 2, axis=0))
-    return np.exp(1j * profile.true_N * np.pi * norm) / norm
+    return np.exp(-1j * profile.true_N * np.pi * norm) / norm
 
 def AK(profile, A, coords_list):
     """Carry out tensor compression along the axes of A and the first axes of the subtensor of K given by coords_list."""
